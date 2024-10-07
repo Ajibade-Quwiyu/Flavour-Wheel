8a216056aa758281a76c85161bee8e7c080a4f7d
Ajibade Quwiyu <>
2024-10-07 10:20:33 +0100

    canvas scaler fix

--- Assets/Keyboard Package/Scripts/KeyboardButtonController.cs
+++ Assets/Keyboard Package/Scripts/KeyboardButtonController.cs
@@ -1,8 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Collections;
 
 public class KeyboardButtonController : MonoBehaviour
 {
@@ -12,40 +11,88 @@ public class KeyboardButtonController : MonoBehaviour
     [SerializeField] TextMeshProUGUI containerText;
     [SerializeField] TextMeshProUGUI containerActionText;
 
-    private void Start() {
+    private float clickAnimationDuration = 0.15f;
+    private float clickScaleMultiplier = 1.5f;
+
+    private Vector3 originalScale;
+
+    private void Start()
+    {
         SetContainerBorderColor(ColorDataStore.GetKeyboardBorderColor());
         SetContainerFillColor(ColorDataStore.GetKeyboardFillColor());
         SetContainerTextColor(ColorDataStore.GetKeyboardTextColor());
         SetContainerActionTextColor(ColorDataStore.GetKeyboardActionTextColor());
+        originalScale = transform.localScale;
     }
 
     public void SetContainerBorderColor(Color color) => containerBorderImage.color = color;
     public void SetContainerFillColor(Color color) => containerFillImage.color = color;
     public void SetContainerTextColor(Color color) => containerText.color = color;
-    public void SetContainerActionTextColor(Color color) { 
+    public void SetContainerActionTextColor(Color color)
+    {
         containerActionText.color = color;
         containerIcon.color = color;
     }
 
-    public void AddLetter() {
-        if(GameManager.Instance != null) {
+    public void AddLetter()
+    {
+        StartCoroutine(ClickAnimation());
+        if (GameManager.Instance != null)
+        {
             GameManager.Instance.AddLetter(containerText.text);
-        } else {
+        }
+        else
+        {
             Debug.Log(containerText.text + " is pressed");
         }
     }
-    public void DeleteLetter() { 
-        if(GameManager.Instance != null) {
+
+    public void DeleteLetter()
+    {
+        StartCoroutine(ClickAnimation());
+        if (GameManager.Instance != null)
+        {
             GameManager.Instance.DeleteLetter();
-        } else {
+        }
+        else
+        {
             Debug.Log("Last char deleted");
         }
     }
-    public void SubmitWord() {
-        if(GameManager.Instance != null) {
+
+    public void SubmitWord()
+    {
+        StartCoroutine(ClickAnimation());
+        if (GameManager.Instance != null)
+        {
             GameManager.Instance.SubmitWord();
-        } else {
+        }
+        else
+        {
             Debug.Log("Submitted successfully!");
         }
+    }
+
+    private IEnumerator ClickAnimation()
+    {
+        // Scale up
+        float elapsedTime = 0;
+        while (elapsedTime < clickAnimationDuration / 2)
+        {
+            transform.localScale = Vector3.Lerp(originalScale, originalScale * clickScaleMultiplier, elapsedTime / (clickAnimationDuration / 2));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        // Scale down
+        elapsedTime = 0;
+        while (elapsedTime < clickAnimationDuration / 2)
+        {
+            transform.localScale = Vector3.Lerp(originalScale * clickScaleMultiplier, originalScale, elapsedTime / (clickAnimationDuration / 2));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localScale = originalScale;
     }
 }
